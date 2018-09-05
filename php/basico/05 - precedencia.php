11c6023e61a854950a5b8d0ca600dbc251ed0939
voodoo <voodoofabio@gmail.com>
2018-09-05 09:31:31 -0300

    estudos 05 de setembro 2k18

---
+++ php/basico/05 - precedencia.php
@@ -0,0 +1,21 @@
+<?PHP
+$soma = 5 + 3 * 8;
+$soma = 5 + 24;
+$soma = 29;
+
+echo "$soma <hr>";
+
+
+$string = "School of Net"."Iniciando com PHP";
+$string = "School of Net Iniciando com PHP";
+
+echo 5 + 3 * 8 . "<hr>";
+
+
+$soma = (5 + 3) * 8;
+
+echo "$soma <hr>";
+
+$soma = (5 + 3 * 5 ) * 8;
+
+echo "$soma <hr>";
