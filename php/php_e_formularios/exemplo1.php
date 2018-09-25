ac1473a3b6ac037330aacbd75710c91d75b6fb20
Fabio <voodoofabio@gmail.com>
2018-09-25 07:00:18 -0300

    estudo ter, 25 de setembro de 2018

---
+++ php/php_e_formularios/exemplo1.php
@@ -0,0 +1,33 @@
+<?php
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST'){
+    echo "O formulario foi enviado pelo ".$_POST['nome'];
+    exit;
+}
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <form action="" method="post">
+        <input type="text" name="nome">
+        <input type="submit" value="enviar">
+    </form>
+
+<a href="index.php?name=fabio">nome</a>
+
+<?php 
+if (isset($_GET['name']) != false ){
+
+}
+    
+?>
+
+</body>
+</html>
---
+++ php/php_e_formularios/index.php
@@ -0,0 +1,29 @@
+<?php 
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST'){
+    $idade = (int)$_POST['idade'];
+    
+    if ($idade < 18){
+        die('Voce é muito novo');
+    }
+    echo 'ok sua idade é ' . $idade;
+        exit;
+}
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <form action="" method="post">
+        <input type="text" name="idade" placeholder="idade">
+        <input type="submit" value="enviar">
+    </form>
+
+</body>
+</html>
