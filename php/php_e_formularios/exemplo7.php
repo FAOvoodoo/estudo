a5109caed1b434c81c10472062ed3e57f612655c
Fabio <voodoofabio@gmail.com>
2018-09-25 10:13:52 -0300

    Estudo PHP e Formularios - final

--- php/php_e_formularios/exemplo7.php
+++ php/php_e_formularios/exemplo7.php
@@ -1,13 +1,12 @@
 <?php
-//REQUEST
-
+//isset vs empty
+//isset => o campo existe?
+//empty => o campo estavazio?
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST'){
 
-    var_dump($_POST);
-    var_dump($_GET);  
-    var_dump($_REQUEST);
-    var_dump(filter_input(INPUT_GET, 'page'));
+    var_dump(isset($_POST['nome']));
+    var_dump(empty($_POST['nome']));  
     exit;
 }
 
@@ -21,11 +20,10 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST'){
     <title>Document</title>
 </head>
 <body>
-    <form action="index.php?page=sobre-nos" method="post">
+    <form action="" method="post">
     <input type="text" name="nome">
-    <input type="submit" value="enviar">
+        <input type="submit" value="enviar">
     </form>
-
 
 </body>
 </html>
