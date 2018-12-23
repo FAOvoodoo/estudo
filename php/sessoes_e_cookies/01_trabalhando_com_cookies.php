7454e77e5a47a842dfe5ec97aa850735ea017193
voodoo <voodoofabio@gmail.com>
2018-12-23 10:22:37 -0200

    estudo

---
+++ php/sessoes_e_cookies/01_trabalhando_com_cookies.php
@@ -0,0 +1,3 @@
+<?php
+setcookie('meunome','FABIO',time()+3600);
+?>
---
+++ php/sessoes_e_cookies/index.php
@@ -0,0 +1,11 @@
+<html>
+<head></head>
+<body>
+
+<h1><ul>
+<li><a href="01_trabalhando_com_cookies.php">Trabalhando com Cookies</a></li>
+</ul></h1>
+     
+</body>
+</html>
+<!-- echo "<a href=\"index.php\">Inicio</a><br><br><br>" -->
